1c740d3c295fb7d82cf69a85c65bca61bc140c47
MicraKS <98130345+MicraKS@users.noreply.github.com>
2023-09-26 18:02:07 +0300

    Formatted the code for better readability

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 import random
 
 
-def get_length_list(n):  # Сгенирируем n-ключей, они будут использованы как размер каждого массива
+def get_length_list(n):  # Generate n keys, they will be used as the size of each array.
     len_list = set()
+
     while len(len_list) < n:
         for i in range(n):
             len_list.add(random.randint(1, n))
+
     len_list = list(len_list)
     random.shuffle(len_list)
+
     return len_list
 
 
 def main(n):
     length_list = get_length_list(n)
     nested_array = []
+
     for i in range(n):
         result_list = []
-        for j in range(length_list[i]):  # Заполняем массив случайными числами
+
+        # Fill the array with random numbers.
+        for j in range(length_list[i]):
             result_list.append(random.randint(1, n))
-        if i % 2 == 0:  # Проверяем массивы на четность/нечетность и сортируем
+
+        # Check the arrays for even/oddness and sort them.
+        if i % 2 == 0:
             result_list.sort()
             nested_array.append(result_list)
         else:
             result_list.sort(reverse=True)
             nested_array.append(result_list)
+
     return nested_array
